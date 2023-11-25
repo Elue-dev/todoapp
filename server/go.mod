effeb6535a532f2b987bfe7857aa8f2f852105aa
Wisdom <eluewisdom@gmail.com>
2023-11-25 18:38:51 +0100

    feat: setup todo structs

--- server/go.mod
+++ server/go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
---
+++ server/models/models.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"title"`
+	Description string `json:"description"`
+	IsCompleted bool `json:"is_completed"`
+}
+
+type CustomTodo struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt gorm.DeletedAt  `json:"deleted_at"`
+	Title string `json:"title"`
+	Description string `json:"description"`
+	IsCompleted bool `json:"is_completed"`
+}
